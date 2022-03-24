70eb5ff7fda968869a0d0d8a1ef0df97bcb6f87e
cindyhanae <cindy.hanae1@gmail.com>
2022-03-24 09:11:50 -0300

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 const gulp = require("gulp")
 
 const sass = require("gulp-sass")(require("sass"))
+const autoprefixer = require("gulp-autoprefixer")
 
 function compileSass(){
   return gulp.src("scss/*.scss")
   .pipe(sass({outputStyle : 'compressed'}))
+  .pipe(autoprefixer({
+    overrideBrowserslist: ["last 2 versions"],
+    cascade: false
+  }))
   .pipe(gulp.dest("css/"))
 }
 
 // task list
-gulp.task("default", compileSass)
+gulp.task("sass", compileSass)
+
+function watch(){
+  gulp.watch("scss/*.scss", compileSass)
+}
+
+gulp.task("default", watch)
