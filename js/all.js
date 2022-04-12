49899be62b087b029afe4da6d6024eaa1678ecc9
cindyhanae <cindy.hanae1@gmail.com>
2022-04-12 10:01:12 -0300

    slider

--- js/all.js
+++ js/all.js
@@ -1,5 +1,18 @@
 "use strict";
 
+var navSlider = new Swiper(".nav-slider", {
+  slidesPerView: 5,
+  direction: "vertical",
+  spaceBetween: 20,
+  watchSlidesProgress: true
+});
 var slideHero = new Swiper(".slider-hero", {
-  effect: "fade"
+  effect: "fade",
+  thumbs: {
+    swiper: navSlider
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  }
 });
